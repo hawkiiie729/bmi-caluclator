80af2513809f76eeb88fb8256bba8e3877adb0e7
hawkiiie729 <mishra.firehawk729@gmail.com>
2022-06-23 23:16:32 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -11,28 +11,69 @@ function App() {
   const [bmi,setBmi]=useState('');
   const [message,setMessage]=useState('');
 
-  let imgSrc='';
+  
 
+  let calcBmi=(event)=>{
+     //prevent submitting
+     event.preventDefault();
 
+     if(weight===0 || height===0){
+      alert("Please enter a valid weight and height")
+     }
+     else{
+      let bmi=(weight/(height*height)*703)
+      setBmi(bmi.toFixed(1));
+ 
+     //logic for message
+     if(bmi<25){
+      setMessage('You are Underweight')
+     }else if(bmi>=25 && bmi<30){
+      setMessage("Yo are a healthy weight")
+     }else{
+      setMessage("You are Overweight")
+     }
 
+     }
+  }
+ 
+  //show image basedon emi calculations
+  let imgSrc;
+
+  if(bmi<1){
+    imgSrc=null;
+  }else {
+    if(bmi <25){
+      imgSrc=require('../src/assets/underweight.png')
+    }
+    else if(bmi>=25 && bmi<30){
+      imgSrc=require('../src/assets/healthy.png')
+    }
+    else{
+      imgSrc=require('../src/assets/overweight.png')
+    }
+  }
+
+  let reload=()=>{
+    window.location.reload()
+  }
 
 
   return (
    <div className="app">
     <div className="container">
   <h2 className="center">BMI Calculator</h2>
-  <form>
+  <form onSubmit={calcBmi}>
     <div>
       <label>Weight (lbs)</label>
-      <input value={weight}/>
+      <input value={weight} onChange={(e)=>setWeight(e.target.value)}/>
     </div>
     <div>
       <label>Height (in)</label>
-      <input value={weight}/>
+      <input value={height} onChange={(e)=>setHeight(e.target.value)}/>
     </div>
     <div>
       <button className="btn" type="submit">Submit</button>
-      <button className="btn btn-outline" type="submit">Submit</button>
+      <button className="btn btn-outline" onClick={reload} type="submit">Reload</button>
     </div>
   </form>
 
