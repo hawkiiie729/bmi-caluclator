2064885ff2e4de8c126ed792a67db6c7b7b329e4
hawkiiie729 <mishra.firehawk729@gmail.com>
2022-06-23 15:27:43 +0530

    created basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from "react";
+
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="app">
+    <div className="container">
+  <h2 className="center">BMI Calculator</h2>
+  <form>
+    <div>
+      <label>Weight (lbs)</label>
+      <input value={weight}/>
     </div>
+    <div>
+      <label>Height (in)</label>
+      <input value={weight}/>
+    </div>
+    <div>
+      <button className="btn" type="submit">Submit</button>
+      <button className="btn btn-outline" type="submit">Submit</button>
+    </div>
+  </form>
+  <div className="center">
+    <h3>Your BMI is: {bmi}</h3>
+    <p>{message}</p>
+  </div>
+
+  <div className="img-container">
+    <img src={imgSrc} alt=''></img>
+  </div>
+    </div>
+   </div>
   );
 }
 
